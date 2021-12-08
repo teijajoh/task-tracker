0f898bf8880d554680c686736a8ac7ccbdaca521
Teija Johansson <teija.johansson50@gmail.com>
2021-12-08 15:46:40 +0100

    commit

--- src/main-page/Tasks.jsx
+++ src/main-page/Tasks.jsx
@@ -1,6 +1,9 @@
 import PropTypes from "prop-types";
 
 function Tasks({ tasks }) {
+    if (!tasks || tasks.length === 0) {
+        return <p>No tasks to display</p>;
+    }
     return tasks.map((task) => 
         <p key={task.id}>{task.text}</p>);
 }
