2ac4821769ad4d0f48e8b481c1f8042262b7c27f
Teija Johansson <teija.johansson50@gmail.com>
2021-12-08 15:05:25 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-
+import Header from "./main-page/Header";
+const initialTasks = [
+  {
+    id: 1,
+    text: "Task 1",
+    day: "Dec 10th at 2:00pm",
+    remainder: true,
+  },
+  
+  {
+    id: 2,
+    text: "Task 2",
+    day: "Dec 10th at 5:00pm",
+    remainder: false,
+  },
+   
+   {
+    id: 3,
+    text: "Task 3",
+    day: "Dec 13th at 6:00pm",
+    remainder: false,
+  }
+];
 
 function App() {
   return (
-    <div>
-      <h1>Hello world : ) </h1>
+    <div className ="container">
+      <Header/> 
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/main-page/Button.jsx
@@ -0,0 +1,26 @@
+import { Button } from "bootstrap";
+import PropTypes from "prop-types";
+
+function Button({children,onClick,color}){
+    return (
+        <button
+            style={{ backgroundColor: "color" }}
+            onClick={onClick}
+            className="btn"
+        >
+            {children}
+        </button>;
+    );
+}
+
+Button.defaultProps = {
+    color:"steelblue",
+}
+
+Button.propTypes = {
+    children: PropTypes.node,
+    onClick: PropTypes.func,
+    color: PropTypes.string,
+};
+
+export default Button;
