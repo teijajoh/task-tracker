085b1a78eae9da6c5967f1a14abf681b002218cf
Teija Johansson <teija.johansson50@gmail.com>
2021-12-09 14:45:23 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ const initialTasks = [
     id: 1,
     text: "Task 1",
     day: "Dec 10th at 2:00pm",
-    remainder: true,
+    reminder: true,
   },
   
   {
     id: 2,
     text: "Task 2",
     day: "Dec 10th at 5:00pm",
-    remainder: false,
+    reminder: false,
   },
    
    {
     id: 3,
     text: "Task 3",
     day: "Dec 13th at 6:00pm",
-    remainder: false,
+    reminder: true,
   }
 ];
 
